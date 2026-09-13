079083e3529a01228b42d375eb8f4a5df50b50a3
Sanobar <chsanobar17@gmail.com>
2026-09-13 21:52:18 +0530

    Reversed the board

--- SNL.js
+++ SNL.js
@@ -1,8 +1,20 @@
 const board = document.getElementById("board");
-for(let i=1;i<=100;i++)
+for(let row=9;row>=0;row--)
 {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.textContent = i;
-    board.appendChild(cell);
+    for(let col=0;col<10;col++)
+    {
+        let number;
+        if(row%2==0)
+        {
+            number = row * 10 + col + 1;
+        }
+        else
+        {
+            number = row * 10 + (10-col);
+        }
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.textContent = number;
+        board.appendChild(cell);
+    }
 }
